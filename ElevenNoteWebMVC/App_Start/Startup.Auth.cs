af0c9a1bb3d71b0423d7a41b145879573b3fbe51
Savannah Jackson <savannahjackson33@gmail.com>
2022-01-19 19:00:01 -0500

    Getting rid of misc. errors

--- ElevenNoteWebMVC/App_Start/Startup.Auth.cs
+++ ElevenNoteWebMVC/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using ElevenNoteWebMVC.Models;
+using ElevenNoteWebMVC.Data;
 
 namespace ElevenNoteWebMVC
 {
--- ElevenNoteWebMVC/Controllers/AccountController.cs
+++ ElevenNoteWebMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ElevenNoteWebMVC.Models;
+using ElevenNoteWebMVC.Data;
 
 namespace ElevenNoteWebMVC.Controllers
 {
