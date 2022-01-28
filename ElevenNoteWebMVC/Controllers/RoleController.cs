13af0aaf3bdc0bf0dfad2eacf673221aa0bbdd4d
Savannah Jackson <savannahjackson33@gmail.com>
2022-01-27 19:00:17 -0500

    RoleFunctionality

---
+++ ElevenNoteWebMVC/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using ElevenNoteWebMVC.Data;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteWebMVC.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext _db;
+
+        public RoleController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = _db.Roles.ToList();
+            return View(Roles);
+        }
+
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        public ActionResult Create(IdentityRole Role)
+        {
+            _db.Roles.Add(Role);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
